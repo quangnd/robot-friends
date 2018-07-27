9c5aa07e296a2c264014ba8af5913c69ac977415
Quang Nguyen <quangnd.edu@gmail.com>
2018-07-27 15:01:21 +0700

    Initial commit

---
+++ src/containers/App.js
@@ -0,0 +1,56 @@
+import React, {Component} from 'react';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import './App.css';
+import Scroll from '../components/Scroll';
+import ErrorBoundry from '../components/ErrorBoundry';
+
+class App extends Component {
+	constructor() {
+		super();
+
+		this.state = {
+			robots: [],
+			searchfield: ''
+		}
+
+		this.onSearchChange = this.onSearchChange.bind(this);
+	}
+
+	componentDidMount() {
+		setTimeout(() => {
+			fetch('https://jsonplaceholder.typicode.com/users')
+			.then(response => response.json())
+			.then(users => { this.setState({ robots: users }) })
+		}, 200)
+
+	}
+
+	onSearchChange(event) {
+		this.setState({
+			searchfield: event.target.value
+		})
+	}
+ 
+	render() {
+		const { searchfield, robots } = this.state;
+		const filteredRobots = robots.filter(robot => {
+			return robot.name.toLowerCase().includes(searchfield.toLowerCase())
+		})
+		return !robots.length 
+		  ? <h1>Loading...</h1>
+			: ( 
+					<div className='tc'>
+						<h1 className='f1'>RoboFriends</h1>
+						<SearchBox searchChange={this.onSearchChange}/>
+						<Scroll>
+							<ErrorBoundry>
+								<CardList robots={filteredRobots} />
+							</ErrorBoundry>
+						</Scroll>
+					</div>
+				)
+	}
+}
+
+export default App;
---
+++ src/robots.js
@@ -0,0 +1,44 @@
+export const robots = [
+{
+	id: 1,
+	name: 'Truong Nguyen',
+	username: 'Lee',
+	email: 'truong@gmail.com'
+},
+{
+	id: 2,
+	name: 'Tuan Le 2',
+	username: 'Lee',
+	email: 'truong@gmail.com'
+},
+{
+	id: 3,
+	name: 'Quang Nguyen 3',
+	username: 'Lee',
+	email: 'truong@gmail.com'
+},
+{
+	id: 4,
+	name: 'Hieu Nguyen 4',
+	username: 'Lee',
+	email: 'truong@gmail.com 5'
+},
+{
+	id: 5,
+	name: 'Anh Nguyen',
+	username: 'Lee',
+	email: 'truong@gmail.com'
+},
+{
+	id: 6,
+	name: 'Anh em',
+	username: 'Anh',
+	email: 'truong@gmail.com'
+},
+{
+	id: 7,
+	name: 'Quang Minh',
+	username: 'Minh',
+	email: 'truong@gmail.com'
+},
+];
